f7ad322c8565be0c87972a8b17f2f21d0b4a94fa
akiufmi139 <akifumi139@gmail.com>
2022-10-12 22:33:51 +0900

    add:CardShapeレイアウトの追加

---
+++ storybook/components/atoms/cardShape/CardShape.stories.tsx
@@ -0,0 +1,12 @@
+
+import { ComponentMeta } from '@storybook/react';
+
+import { CardShape } from './CardShape';
+
+export default {
+    title: 'Example/Atom/CardShape',
+    component: CardShape,
+} as ComponentMeta<typeof CardShape>;
+
+export const Basic = {
+}
---
+++ storybook/components/atoms/cardShape/CardShape.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './cardShape.css';
+
+
+export const CardShape = () => {
+    return (
+        <div className="card-shape"></div>
+    )
+}
